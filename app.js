887840eb11a72277ed326f9206996c3bcac55531
Tom Mitchell <mitcht032@gmail.com>
2020-10-13 22:15:36 -0700

    added files

---
+++ app.js
@@ -0,0 +1,181 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+// Write code to use inquirer to gather information about the development team members,
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?"
+    }, 
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?"
+    },
+    {
+      type: "input",
+      name: "empID",
+      message: "What is your team member ID number?"
+    },
+    { 
+    type: "list",
+    name: "role",
+    message: "What is your role with our team?",
+        choices: ["Manager","Engineer","Intern"]
+    },
+  ]);
+}
+class Employee {
+    // method which prints all of the stats for a employee
+    constructor(name,id,email,role) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+      this.role = role;
+    }
+getName(){
+
+}
+
+getId(){
+
+}
+
+getEmail(){
+
+}
+
+getRole(){
+    
+}
+}
+// Create new employees using the "Employee" class...might have to push new role into arrray
+const employees = [
+new Employee("employee name", 50, "email"),
+new Employee("dean", 60, "email")
+]
+
+module.exports = {
+    Employee,
+    employees
+  };
+  
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+function generateHTML(answers) {
+    return `
+    ` 
+  ;
+  }
+  promptUser()
+    .then(function(answers) {
+      const html = generateHTML(answers);
+  
+      return writeFileAsync("home.html", html);
+    })
+    .then(function() {
+      console.log("Successfully wrote to home.html");
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
+const path = require("path");
+const fs = require("fs");
+
+const templatesDir = path.resolve(__dirname, "../templates");
+
+const render = employees => {
+  const html = [];
+
+  html.push(employees
+    .filter(employee => employee.getRole() === "Manager")
+    .map(manager => renderManager(manager))
+  );
+  html.push(employees
+    .filter(employee => employee.getRole() === "Engineer")
+    .map(engineer => renderEngineer(engineer))
+  );
+  html.push(employees
+    .filter(employee => employee.getRole() === "Intern")
+    .map(intern => renderIntern(intern))
+  );
+
+  return renderMain(html.join(""));
+
+};
+
+const renderManager = manager => {
+  let template = fs.readFileSync(path.resolve(templatesDir, "manager.html"), "utf8");
+  template = replacePlaceholders(template, "name", manager.getName());
+  template = replacePlaceholders(template, "role", manager.getRole());
+  template = replacePlaceholders(template, "email", manager.getEmail());
+  template = replacePlaceholders(template, "id", manager.getId());
+  template = replacePlaceholders(template, "officeNumber", manager.getOfficeNumber());
+  return template;
+};
+
+const renderEngineer = engineer => {
+  let template = fs.readFileSync(path.resolve(templatesDir, "engineer.html"), "utf8");
+  template = replacePlaceholders(template, "name", engineer.getName());
+  template = replacePlaceholders(template, "role", engineer.getRole());
+  template = replacePlaceholders(template, "email", engineer.getEmail());
+  template = replacePlaceholders(template, "id", engineer.getId());
+  template = replacePlaceholders(template, "github", engineer.getGithub());
+  return template;
+};
+
+const renderIntern = intern => {
+  let template = fs.readFileSync(path.resolve(templatesDir, "intern.html"), "utf8");
+  template = replacePlaceholders(template, "name", intern.getName());
+  template = replacePlaceholders(template, "role", intern.getRole());
+  template = replacePlaceholders(template, "email", intern.getEmail());
+  template = replacePlaceholders(template, "id", intern.getId());
+  template = replacePlaceholders(template, "school", intern.getSchool());
+  return template;
+};
+
+const renderMain = html => {
+  const template = fs.readFileSync(path.resolve(templatesDir, "main.html"), "utf8");
+  return replacePlaceholders(template, "team", html);
+};
+
+const replacePlaceholders = (template, placeholder, value) => {
+  const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
+  return template.replace(pattern, value);
+};
+
+module.exports = render;
